9fe9d3e07327c20b19aaa83489b72e3757d88251
yamaji <haruyuki.program@gmail.com>
2023-08-15 10:12:21 +0900

    必要そうなファイルを追加

---
+++ public/main.js
@@ -0,0 +1,1 @@
+document.querySelector('body').innerHTML = '<h1>タイトル</h1>';
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
 
+import quiz from "./quiz.json" assert {type: "json"};
+
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
   if (req.method === "GET" && pathname === "/welcome-message") {
     return new Response("jigインターンへようこそ！");
+  }
+
+  if (req.method === "GET" && pathname.startsWith("/quiz/") ) {
+    // console.log("fire", req.url, pathname);
+    let id = pathname.replace("/quiz/", "");
+    console.log(id);
+
+    return new Response(JSON.stringify(quiz[id]));
   }
 
   return serveDir(req, {
