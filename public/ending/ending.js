bb2a0d428a1249856d4146886946bbc556032526
Daisuke0655 <85994195+Daisuke0655@users.noreply.github.com>
2023-08-17 14:03:18 +0900

    ending完成
    ending完成

--- public/ending/ending.js
+++ public/ending/ending.js
@@ -1,5 +1,14 @@
-const startButton = document.querySelector("button")
-startButton.addEventListener("click", () => {
+const pushToQuiz = document.getElementById("pushToQuiz")
+const pushToIndex = document.getElementById("pushToIndex")
+const tmp2 = 30;
+const scoreText = "Your Score = " + tmp2;
+pushToQuiz.addEventListener("click", () => {
+  console.log("fire")
+  window.location.href = "/quiz.html"
+})
+pushToIndex.addEventListener("click", () => {
   console.log("fire")
   window.location.href = "/index.html"
 })
+const score = document.getElementById("score")
+score.textContent = scoreText;
--- public/ending/gameover.js
+++ public/ending/gameover.js
@@ -1,3 +1,3 @@
-const tmp = "台風に巻き込まれてあなたは死んでしまった";
-const message = document.querySelector("p");
-message.textContent = tmp;
+const tmp1 = "台風に巻き込まれてあなたは死んでしまった";
+const message = document.getElementById("message");
+message.textContent = tmp1;
