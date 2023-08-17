38510b03c040809ba2b4e477d203feab19582e7b
NKMAK <nakamuraakira18@gmail.com>
2023-08-17 13:57:33 +0900

    gameover.htmlでend.jsonの要素を取得する機能　基礎
    gameover.htmlでend.jsonの要素を取得する機能　基礎

--- public/ending/ending.js
+++ public/ending/ending.js
@@ -1,2 +1,6 @@
+import{getEndJson} from"./getQuizEnd.js";
+
+let endData= await getEndJson(0);
+console.log(endData);
 const tmp = "災害で死んでしまった";
 document.getElementById("message").textContent = tmp;
