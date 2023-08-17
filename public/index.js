8a0b903717c146adb8afec2dbec7835aa45ebff0
Daisuke0655 <85994195+Daisuke0655@users.noreply.github.com>
2023-08-17 15:24:13 +0900

    indexページのデザイン修正とquizpageへの遷移
    indexページのデザイン修正とquizpageへの遷移

---
+++ public/index.js
@@ -0,0 +1,5 @@
+const startButton = document.querySelector("button")
+startButton.addEventListener("click", () => {
+  console.log("fire")
+  window.location.href = "/typhoonQuiz/typhoonQuiz.html"
+})
